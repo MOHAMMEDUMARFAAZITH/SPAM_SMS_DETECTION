2925cd9c35d26517c6a7d36b98bfd4ffaba99367
MOHAMMED UMAR FAAZITH K <123172380+MOHAMMEDUMARFAAZITH@users.noreply.github.com>
2023-06-08 22:22:06 +0530

    Update SPAM_CLASSIFICATION_PROJECT.py

--- SPAM_CLASSIFICATION_PROJECT.py
+++ SPAM_CLASSIFICATION_PROJECT.py
@@ -11,17 +11,6 @@ df["Target"] = encoder.fit_transform(df["Target"])
 df = df.drop_duplicates(keep="first")
 import nltk
 nltk.download('punkt')
-import nltk
-import ssl
-
-try:
-    _create_unverified_https_context = ssl._create_unverified_context
-except AttributeError:
-    pass
-else:
-    ssl._create_default_https_context = _create_unverified_https_context
-
-nltk.download()
 from nltk.corpus import stopwords
 import string
 from nltk.stem.porter import PorterStemmer
