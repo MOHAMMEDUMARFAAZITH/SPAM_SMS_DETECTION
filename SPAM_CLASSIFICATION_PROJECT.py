53707ceacdead03e5106b34181e8c782ba6da8a7
MOHAMMED UMAR FAAZITH K <123172380+MOHAMMEDUMARFAAZITH@users.noreply.github.com>
2023-06-08 15:38:29 +0530

    Add files via upload

---
+++ SPAM_CLASSIFICATION_PROJECT.py
@@ -0,0 +1,75 @@
+import streamlit as st
+import numpy
+import pandas as pd
+df = pd.read_csv(r"C:\Users\RAZITH\Downloads\spam (1).csv",encoding='cp1252')
+df.drop(columns=["Unnamed: 2","Unnamed: 3","Unnamed: 4"],inplace=True)
+df.rename(columns={"v1":"Target","v2":"Text"},inplace=True)
+from sklearn.preprocessing import LabelEncoder
+encoder = LabelEncoder()
+df["Target"] = encoder.fit_transform(df["Target"])
+df = df.drop_duplicates(keep="first")
+import nltk
+from nltk.corpus import stopwords
+import string
+from nltk.stem.porter import PorterStemmer
+ps = PorterStemmer()
+def transform_text(text):
+    text = text.lower()
+    text = nltk.word_tokenize(text)
+    
+    y = []
+    for i in text:
+        if i.isalnum():
+            y.append(i)
+    
+    text = y[:]
+    y.clear()
+    
+    for i in text:
+        if i not in stopwords.words('english') and i not in string.punctuation:
+            y.append(i)
+            
+    text = y[:]
+    y.clear()
+    
+    for i in text:
+        y.append(ps.stem(i))
+    
+            
+    return " ".join(y)
+df['transformed_text'] = df['Text'].apply(transform_text)
+from sklearn.feature_extraction.text import TfidfVectorizer
+tfidf = TfidfVectorizer(max_features=3000)
+X = tfidf.fit_transform(df['transformed_text']).toarray()
+y = df['Target'].values
+from sklearn.model_selection import train_test_split
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=2)
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score,confusion_matrix,precision_score
+mnb = MultinomialNB()
+
+mnb.fit(X_train,y_train)
+y_pred2 = mnb.predict(X_test)
+print(accuracy_score(y_test,y_pred2))
+print(confusion_matrix(y_test,y_pred2))
+print(precision_score(y_test,y_pred2))
+print(y_pred2)
+
+
+st.title("Email/SMS Spam Classifier")
+
+input_sms = st.text_area("Enter the message")
+
+if st.button('Predict'):
+
+    # 1. preprocess
+    transformed_sms = transform_text(input_sms)
+    # 2. vectorize
+    vector_input = tfidf.transform([transformed_sms])
+    # 3. predict
+    result = mnb.predict(vector_input)[0]
+    # 4. Display
+    if result == 1:
+        st.header("Spam")
+    else:
+        st.header("Not Spam")
